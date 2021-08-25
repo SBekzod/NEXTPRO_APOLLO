caa45c4b0358e40b9fabc73eb848cf90d2211824
Martin <bekzodsanakulov89@gmail.com>
2021-08-25 23:01:35 +0900

    feat: link rotes features

--- pages/table/[tableId].js
+++ pages/table/[tableId].js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import { ChannelList } from "../../apollo/channel";
 import ChannelComponent from "../../components/channelCompo";
+import Link from "next/link";
 
 export default function GetStartedPage() {
   const { query } = useRouter();
@@ -18,11 +19,15 @@ export default function GetStartedPage() {
 
   if (data && !dataObj) {
     setDataObj(data);
+    console.log('*********************')
   }
 
   return (
     <div>
       <p>THIS IS JUST A TABLE {query.tableId}</p>
+      <Link href="/table">
+        <a style={{color: "green", fontSize: 24}}>GO</a>
+      </Link>
       {dataObj !== null &&
         dataObj.channels.map((ele) => {
           console.log(ele.channel_id);
--- pages/table/index.js
+++ pages/table/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Link from 'next/link'
 
 export default function GetStartedPage() {
   return <div>
       <p>TABLE 1</p>
       <p>TABLE 2</p>
       <p>TABLE 3</p>
+      <Link href="/table/5">
+        <a style={{color: "green"}}>PRESS</a>
+      </Link>
   </div>
 }
